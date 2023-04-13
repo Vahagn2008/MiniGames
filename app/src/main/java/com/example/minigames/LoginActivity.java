dca6de647f170d01fc5fd415595fd62bd64b506e
Vahagn <vahagn.1dallakyan@gmail.com>
2023-04-13 20:48:40 +0400

    Second Project

--- app/src/main/java/com/example/minigames/LoginActivity.java
+++ app/src/main/java/com/example/minigames/LoginActivity.java
@@ -1,22 +1,100 @@
 package com.example.minigames;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+    TextView btn;
+    EditText inputEmail, inputPassword;
+    Button btnLogin;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mLoadingBar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn = findViewById(R.id.textViewSignUp);
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        btnLogin = findViewById(R.id.btnlogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkCrededentials();
+            }
+
+            private void checkCrededentials() {
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+
+                mAuth = FirebaseAuth.getInstance();
+                mLoadingBar = new ProgressDialog(LoginActivity.this);
+
+
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                    }
+                });
+
+
+                if (email.isEmpty() || !email.contains("@"))
+                {
+                    showError(inputEmail, "Email is not valid");
+                }
+                else if (password.isEmpty() || password.length()<7)
+                {
+                    showError(inputPassword, "Password must be 7 character");
+                }
+                else
+                {
+                    mLoadingBar.setTitle("Login");
+                    mLoadingBar.setMessage("Please wait, while check your credentials");
+                    mLoadingBar.setCanceledOnTouchOutside(false);
+                    mLoadingBar.show();
+
+                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful())
+                            {
+                                mLoadingBar.dismiss();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+                }
+            }
+
+            private void showError(EditText input, String s) {
+                input.setError(s);
+                input.requestFocus();
+            }
+        });
         TextView btn = findViewById(R.id.textViewSignUp);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
             }
         });

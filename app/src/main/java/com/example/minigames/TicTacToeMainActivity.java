bd8d9ac9161fca6f53cf99ede8f8c5528e5cb6a8
Vahagn <vahagn.1dallakyan@gmail.com>
2023-04-15 01:16:54 +0400

    Fourth Project

---
+++ app/src/main/java/com/example/minigames/TicTacToeMainActivity.java
@@ -0,0 +1,26 @@
+package com.example.minigames;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+import com.example.minigames.Fragments.StartFragment;
+
+public class TicTacToeMainActivity extends AppCompatActivity {
+
+    private FrameLayout main_frame;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_tic_tac_toe_main);
+        main_frame = findViewById(R.id.main_frame);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_frame, new StartFragment());
+        transaction.commit();
+    }
+}

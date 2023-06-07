d903a57f03ebb775b2ac6064b91cd15f2b37bf60
Vahagn Dallakyan <vahagn.1dallakyan@gmail.com>
2023-06-07 21:28:13 +0400

    Final Project

--- app/src/main/java/com/example/minigames/Memory.java
+++ app/src/main/java/com/example/minigames/Memory.java
@@ -7,15 +7,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Memory extends AppCompatActivity {
 
+    ImageButton backk;
     Button memory43, memory54;
 
     @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory);
+
+        backk = findViewById(R.id.backk);
+        backk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Memory.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         memory43 = (Button) findViewById(R.id.memory43);
         memory54 = (Button) findViewById(R.id.memory54);
